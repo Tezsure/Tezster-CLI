aa2c4639e26d12184d6b664ad2203a6bc110f328
Jignesh Vasoya <jigneshvasoya.3292@gmail.com>
2019-05-15 23:02:49 +0530

    Created a single script to install and run tezos node

---
+++ script/env.sh
@@ -0,0 +1,5 @@
+SETUP_SUCCESS_FILE="./setup.successful" # This variable gets set from ./tezos directory
+RUN_NODE_SUCCESS_FILE="./run-node.successful"
+TEZOS_REPO_DIRECTORY="./tezos"
+NODE_18731_LOG="./node-18731.log"
+NODE_18732_LOG="./node-18732.log"
--- script/start_network.sh
+++ script/start_network.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
+#TODO : 
+#   - Make sure nodes are running successfully
+
 eval `./tezos/src/bin_client/tezos-init-sandboxed-client.sh 1`
 tezos-activate-alpha
+echo "Tezos chain has been deployed successfully."
