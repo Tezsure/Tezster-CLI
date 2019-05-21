5a372a4b93fcc89902243041c7e78328f3b2f95f
rmanjhi <rmanjhi.cse13@nituk.ac.in>
2019-05-21 13:11:39 +0530

    code changes

--- tezster.js
+++ tezster.js
@@ -9,13 +9,6 @@ defaultConfig = {
   contracts : [],
   programs : [],
 },
-cliColors = {
-  red : '31m',
-  yellow : '33m',
-  cyan : '36m',
-  white : '37m',
-  green : '32m',
-},
 validCommands = [
   'man',
   'help',
