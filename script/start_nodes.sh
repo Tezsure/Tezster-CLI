a9dbda28e3d66106fbc0099a53c53643366bc10e
jvasoya <jignesh@credth.io>
2019-05-29 23:32:40 +0530

    Added keys file to activate alpha + version changes

--- script/start_nodes.sh
+++ script/start_nodes.sh
@@ -22,7 +22,7 @@ function main {
 
 function activateAlpha() {
     ./tezos/src/bin_client/tezos-init-sandboxed-client.sh 1
-    ./tezos/tezos-client -base-dir /tmp/tezos-tmp-client.8ScVVzls -addr localhost -port 18731  -block genesis activate protocol PsddFKi32cMJ2qPjf43Qv5GDWLDPZb3T3bF6fLKiF5HtvHNU7aP with fitness 1 and key activator and parameters ./tezos/scripts/protocol_parameters.json --timestamp $(TZ='AAA+1' date +%FT%TZ)
+    ./tezos/tezos-client -base-dir ./tmp -addr localhost -port 18731  -block genesis activate protocol PsddFKi32cMJ2qPjf43Qv5GDWLDPZb3T3bF6fLKiF5HtvHNU7aP with fitness 1 and key activator and parameters ./tezos/scripts/protocol_parameters.json --timestamp $(TZ='AAA+1' date +%FT%TZ)
 }
 
 main
