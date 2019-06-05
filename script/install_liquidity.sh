4e29fc4bf5a8e4580cbca396159bcb3145176c97
jvtezsure <jignesh@tezsure.com>
2019-06-05 21:00:27 +0530

    liquidity installation fix and version change

--- script/install_liquidity.sh
+++ script/install_liquidity.sh
@@ -2,6 +2,7 @@
 source ./env.sh
 
 function main {
+    export OPAMYES="yes"
     opam switch create liquidity 4.06.1
     eval `opam env --switch liquidity`
     git clone https://github.com/OCamlPro/liquidity
