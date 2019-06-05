f81d2214d9117f368ed516d3782a3dd20a17d177
rajeev kumar <30716153+rmanjhi@users.noreply.github.com>
2019-06-05 19:51:10 +0530

    code changes to add account

--- tezster-manager.js
+++ tezster-manager.js
@@ -103,6 +103,27 @@ function transferAmount(args){
 
 }
 
+function createAccount(args){
+  var pkh = args[0], f;  
+  if (findKeyObj(config.accounts, args[1])) return console.log(outputError("That account name is already in use"));
+  if (f = findKeyObj(config.identities, pkh)) {
+      return eztz.rpc.account(f, parseFloat(args[2]), true, true,f.pkh, 1400).then(function(r){                  
+              var d=eztz.contract.hash(r.hash, 0);        
+              config.accounts.push({
+                label : args[1],
+                pkh : d,
+                identity : pkh,        
+              });
+              jsonfile.writeFile(confFile, config);
+              return output("New account created " + args[1]);
+          }).catch(function(e){           
+              return outputError(e);
+            });
+  } else {
+      return outputError(pkh + " is not a valid identity");
+  }
+}
+
 function findKeyObj(list, t){
     for (var i = 0; i < list.length; i++){
       if (list[i].pkh == t || list[i].label == t) return list[i];
@@ -132,5 +153,6 @@ module.exports= {
     eztz: eztz,
     getProvider: getProvider,
     setProvider: setProvider,
-    transferAmount: transferAmount
+    transferAmount: transferAmount,
+    createAccount:createAccount
 };
