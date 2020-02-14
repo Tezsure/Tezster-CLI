6b986d0908bcf70b8f2ba6ae8fa1a99dda31b3a9
kapiltez <kapil@tezsure.com>
2020-02-14 18:45:05 +0530

    update script files

--- docker/bake_tx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source usr/local/bin/env.sh
-
-function main {
-    cd /tezos && tezos-client -l -base-dir ./tmp -addr localhost -port 18731  bake for $1 >> $BAKING_LOGS
-}
-
-main "$@"
--- script/setup.sh
+++ script/setup.sh
@@ -1,83 +1,24 @@
 #!/bin/bash
-source ./env.sh
 
 function main {
-    sudo apt-get -y update 
-    sudo apt-get -y upgrade
-    cleanup
-    installDependencies
-    installOpamIfNotFound
-    setUpTezosRepoandBuildTezosNode
+    checkDependencies
+    pullDockerImage
 }
 
-function findDependencyAndInstallIfNotFound {
+function findDependency {
     DEPENDENCYTOCHECK=$1
     echo "checking if $DEPENDENCYTOCHECK is installed or not...."
     if ! [ -x "$(command -v $DEPENDENCYTOCHECK)" ]; then
-        echo "Error: ${DEPENDENCYTOCHECK} is not installed." >&2
-        echo "Installing ${DEPENDENCYTOCHECK} ....."
-        sudo apt-get install $DEPENDENCYTOCHECK -y
+        echo "Error: ${DEPENDENCYTOCHECK} is not installed. Make sure that you have ${DEPENDENCYTOCHECK} installed on your system" >&2
     fi
 }
 
-function installOpamIfNotFound {
-    if ! [ -x "$(command -v opam)" ]; then
-        echo "Error: opam is not installed." >&2
-        echo 'Installing Opam...'
-        wget https://github.com/ocaml/opam/releases/download/2.0.0/opam-2.0.0-x86_64-linux
-        sudo mv opam-2.0.0-x86_64-linux /usr/local/bin/opam
-        sudo chmod a+x /usr/local/bin/opam
-    fi
-
-    yes | opam init --compiler=4.06.1 --disable-sandboxing
-    eval $(opam env)
+function pullDockerImage {
+    sudo docker pull kapil1221/tezster-cli
 }
 
-function setUpTezosRepoandBuildTezosNode {
-    findDependencyAndInstallIfNotFound git
-
-    echo 'getting tezos repo...'
-    git clone -b babylonnet https://gitlab.com/tezos/tezos.git
-    cd tezos/
-    yes | make build-deps
-    eval $(opam env)
-    yes | make
-
-    BUILD_FILE="./_build/default/src/bin_node/main.exe"
-    if [ -f "$BUILD_FILE" ]; then
-        touch ".$SETUP_SUCCESS_FILE"
-        echo 'build is successful...'
-    else 
-        echo 'build is not successful, pleaese try again....'
-    fi
-    
-}
-
-function installDependencies {
-    findDependencyAndInstallIfNotFound make
-    findDependencyAndInstallIfNotFound unzip
-    findDependencyAndInstallIfNotFound libev-dev
-    findDependencyAndInstallIfNotFound libgmp-dev
-    findDependencyAndInstallIfNotFound m4
-    findDependencyAndInstallIfNotFound pkg-config
-    findDependencyAndInstallIfNotFound libhidapi-dev
-    findDependencyAndInstallIfNotFound build-essential
-    findDependencyAndInstallIfNotFound nohup
-    findDependencyAndInstallIfNotFound lsof
-}
-
-function cleanup {
-    if [ -f "$SETUP_SUCCESS_FILE" ]; then
-        rm $SETUP_SUCCESS_FILE
-    fi
-
-    if [ -f "$RUN_NODE_SUCCESS_FILE" ]; then
-        rm $RUN_NODE_SUCCESS_FILE
-    fi 
-
-    if [ -d "$TEZOS_REPO_DIRECTORY" ]; then
-        rm -rf $TEZOS_REPO_DIRECTORY
-    fi
+function checkDependencies {
+    findDependency docker
 }
 
 main
