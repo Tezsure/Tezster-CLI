5656fe216c2b4eefc75322cfa285df3e41b42680
kapiltez <kapil@tezsure.com>
2020-02-19 20:12:35 +0530

    commit changes

--- docker/env.sh
+++
@@ -1,9 +0,0 @@
-SETUP_SUCCESS_FILE="./setup.successful" # This variable gets set from ./tezos directory
-RUN_NODE_SUCCESS_FILE="./run-node.successful"
-TEZOS_REPO_DIRECTORY="./tezos"
-NODE_18731_LOG="./node-18731.log"
-NODE_18732_LOG="./node-18732.log"
-NODE_18733_LOG="./node-18733.log"
-ACTIVATE_ALPHA_LOG="./activate-alpha.log"
-BAKING_LOGS="./baking-tx.log"
-BAKER_DATA_DIR="./tmp/tz-18731"
