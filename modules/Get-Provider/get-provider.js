9c2d8104af8b8080b57b22affe9db5bac194a8cc
kapiltez <kapil@tezsure.com>
2020-03-05 13:12:54 +0530

    refactoring update

--- modules/Get-Provider/get-provider.js
+++
@@ -1,9 +0,0 @@
-const tezsterManager = require("../../tezster-manager");
-const getProviderManager = require("./get-provider-manager");
-
-async function __getProvider() {
-    await tezsterManager.loadTezsterConfig(); 
-    console.log(getProviderManager.getProvider());
-}
-
-module.exports = { __getProvider };
