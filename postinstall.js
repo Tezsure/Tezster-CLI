1c81cff52ea1cd0e969b2a2c8789d64536e5a375
kapiltez <kapil@tezsure.com>
2020-06-08 18:15:54 +0530

    windows node-status fix

--- postinstall.js
+++ postinstall.js
@@ -1,4 +1,4 @@
-const { confFile, CONFIG_FILE_ABSOLUTE_PATH_INSIDE_NPM_PACKAGE, TEZSTER_FOLDER_PATH_INSIDE_TEMP } = require('./modules/cli-constants');
+const { confFile, CONFIG_FILE_ABSOLUTE_PATH_INSIDE_NPM_PACKAGE, TEZSTER_FOLDER_PATH_INSIDE_TEMP, TEMP_FOLDER } = require('./modules/cli-constants');
 
 const fs = require('fs'),
 path = require('path');
@@ -6,8 +6,8 @@ path = require('path');
 const pathToFile = path.join(CONFIG_FILE_ABSOLUTE_PATH_INSIDE_NPM_PACKAGE);
 const pathToNewDestination = confFile;
 
-if(!fs.existsSync('/tmp/')) {
-  fs.mkdirSync('/tmp/');
+if(!fs.existsSync(TEMP_FOLDER)) {
+  fs.mkdirSync(TEMP_FOLDER);
 }
 
 if(!fs.existsSync(TEZSTER_FOLDER_PATH_INSIDE_TEMP)) {
