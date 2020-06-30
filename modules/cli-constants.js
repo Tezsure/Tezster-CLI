bc200862b3aab662ed3718452f9f1c2e3395471d
kapiltez <kapil@tezsure.com>
2020-06-30 15:35:04 +0530

    prompt parameters modification

--- modules/cli-constants.js
+++ modules/cli-constants.js
@@ -21,102 +21,11 @@ const confFile = `/var/tmp/tezster/config.json`,
       START_NODES_PROGRESS_BAR_INTERVAL_WIN = 0.80,
       LOCAL_NODE_URL = 'http://localhost:18731',
       WIN_OS_PLATFORM = 'win32',
-      WIN_WSL_OS_RELEASE = 'microsoft',
-
-      deployParameter = [
-        {
-          type : 'input',
-          name : 'contractLabel',
-          message : 'Enter contract label: '
-        },
-        {
-          type : 'input',
-          name : 'contractAbsolutePath',
-          message : 'Enter absolute path of contract(.tz) file: '
-        },
-        {
-          type : 'input',
-          name : 'initStorageValue',
-          message : 'Enter initial storage value: '
-        },
-        {
-            type : 'input',
-            name : 'account',
-            message : 'Enter account label/pkh: '
-        },
-        {
-            type : 'input',
-            name : 'amount',
-            message : 'Enter amount: (optional, default is 0) ',
-            default: 0,
-        },
-        {
-            type : 'input',
-            name : 'fee',
-            message : 'Enter operation fee: (optional, default is 100000)',
-            default: 100000,
-        },
-        {
-            type : 'input',
-            name : 'storageLimit',
-            message : 'Enter storage limit: (optional, default is 10000)',
-            default: 10000,
-        },
-        {
-            type : 'input',
-            name : 'gasLimit',
-            message : 'Enter gas limit: (optional, default is 500000)',
-            default: 500000,
-        },
-    ],
-    
-    callParameter = [
-        {
-          type : 'input',
-          name : 'contractName',
-          message : 'Enter contract name: '
-        },
-        {
-          type : 'input',
-          name : 'argumentValue',
-          message : 'Enter argument value: '
-        },
-        {
-            type : 'input',
-            name : 'account',
-            message : 'Enter account label/pkh: '
-        },
-        {
-            type : 'input',
-            name : 'amount',
-            message : 'Enter amount: (optional, default is 0) ',
-            default: 0,
-        },
-        {
-            type : 'input',
-            name : 'fee',
-            message : 'Enter operation fee: (optional, default is 100000)',
-            default: 100000,
-        },
-        {
-            type : 'input',
-            name : 'storageLimit',
-            message : 'Enter storage limit: (optional, default is 10000)',
-            default: 10000,
-        },
-        {
-            type : 'input',
-            name : 'gasLimit',
-            message : 'Enter gas limit: (optional, default is 500000)',
-            default: 500000,
-        },
-    ];
-      
+      WIN_WSL_OS_RELEASE = 'microsoft'; 
 
 module.exports = { confFile, CONSEIL_JS, TESTNET_NAME, CONSEIL_SERVER_APIKEY, TEZSTER_LOGS_FOLDER_PATH,
                    IMAGE_TAG, CONTAINER_NAME, LOCAL_NODE_URL, CONSEIL_SERVER_URL, COMMAND_LOG_FILE, TEMP_PATH,
                    PROGRESS_REFRESH_INTERVAL, TEZSTER_FOLDER_PATH, START_NODES_PROGRESS_BAR_INTERVAL_WIN,
                    LOGS_ZIPFILE_PATH, LOG_FOLDER_PATH_INSIDE_DOCKER, LOGS_ZIPFILE_NAME,
                    CONFIG_FILE_ABSOLUTE_PATH_INSIDE_NPM_PACKAGE, WIN_OS_PLATFORM, WIN_WSL_OS_RELEASE,
-                   NODE_CONFIRMATION_TIMEOUT_WIN, Node_Confirmation_Timeout, Start_Nodes_Progress_Bar_Interval,
-                   deployParameter, callParameter  };
+                   NODE_CONFIRMATION_TIMEOUT_WIN, Node_Confirmation_Timeout, Start_Nodes_Progress_Bar_Interval };
