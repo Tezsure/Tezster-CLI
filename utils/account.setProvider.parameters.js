45aa501b427c92f3a0254dfbb35225b28c02f634
kapiltez <kapil@tezsure.com>
2020-07-06 12:13:18 +0530

    added prompt for set-provider

--- utils/account.setProvider.parameters.js
+++ utils/account.setProvider.parameters.js
@@ -3,7 +3,7 @@ const accountSetProviderParameters = [
       type : 'list',
       name : 'newNodeProvider',
       message : 'Select new node provider: ',
-      choices: ['http://localhost:18731', 'https://testnet.tezster.tech', 'https://tezos-dev.cryptonomic-infra.tech', 'https://carthagenet.SmartPy.io', 'http://carthagenet.tezos.cryptium.ch:8732']
+      choices: ['http://localhost:18731', 'https://testnet.tezster.tech', 'https://tezos-dev.cryptonomic-infra.tech', 'https://carthagenet.SmartPy.io']
     },
 ];
 
