9de92590eb8bfaa77eb6c3f7aae2851eb3c89f54
kapiltez <kapil@tezsure.com>
2020-07-10 19:08:22 +0530

    added conditional prompt for stop-nodes

---
+++ utils/setup.stopNodes.parameters.js
@@ -0,0 +1,11 @@
+const chalk = require('chalk');
+const setupStopNodesParameters = [
+    {
+      type: 'confirm',
+      name: 'response',
+      message: `${chalk.yellow('Stopping local nodes will remove existing activated accounts and deployed contracts on local node.')}\n  Please continue, if you agree: `,
+      default: false
+    }
+];
+  
+module.exports = { setupStopNodesParameters }
